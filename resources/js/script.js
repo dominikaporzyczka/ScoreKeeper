0045e1ae17604eed1d66b3b5d60a9a9ffb94644e
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-02-09 11:43:08 +0100

    Added reset button

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,7 @@
 var playerOneBtn = document.querySelector("#playerOne");
 var playerTwoBtn = document.querySelector("#playerTwo");
+var resetBtn = document.querySelector("#reset");
+
 var playerOneDisplay = document.querySelector("#playerOneDisplay");
 var playerTwoDisplay = document.querySelector("#playerTwoDisplay");
 var playerOneScore = 0;
@@ -8,31 +10,38 @@ var playerTwoScore = 0;
 var gameOver = false;
 var winningScore = 5;
 
-var resetBtn = document.querySelector("#reset");
-
-
-playerOneBtn.addEventListener("click", function() {
-    if(!gameOver) {
+playerOneBtn.addEventListener("click", function () {
+    if (!gameOver) {
         playerOneScore++;
-            if(playerOneScore === winningScore) {
-                playerOneDisplay.classList.add("winner");
-                gameOver = true;
-            }
+        if (playerOneScore === winningScore) {
+            playerOneDisplay.classList.add("winner");
+            gameOver = true;
+        }
 
         playerOneDisplay.textContent = playerOneScore;
     }
-    
+
 });
 
-playerTwoBtn.addEventListener("click", function() {
-    if(!gameOver) {
+playerTwoBtn.addEventListener("click", function () {
+    if (!gameOver) {
         playerTwoScore++;
-            if(playerTwoScore === winningScore) {
-                playerTwoDisplay.classList.add("winner");
-                gameOver = true;
-            }
+        if (playerTwoScore === winningScore) {
+            playerTwoDisplay.classList.add("winner");
+            gameOver = true;
+        }
         playerTwoDisplay.textContent = playerTwoScore;
     }
-   
+
+});
+
+resetBtn.addEventListener("click", function () {
+    playerOneScore = 0;
+    playerTwoScore = 0;
+    playerOneDisplay.textContent = 0;
+    playerTwoDisplay.textContent = 0;
+    playerOneDisplay.classList.remove("winner");
+    playerTwoDisplay.classList.remove("winner");
+    gameOver = false;
 });
 
